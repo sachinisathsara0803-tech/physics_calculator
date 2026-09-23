1f276ac3b852aa5562e5ccd0ad7238249ec1fb35
Sachini Sathsara <sachinisathsara0803@gmail.com>
2026-09-23 23:11:43 +0530

    Gravitational time dilation adding and upgrating

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import math
 
 app = Flask(__name__)
+G = 6.674e-11
 
 # 1. Home Page
 @app.route('/')
@@ -10,22 +11,13 @@ def home():
 
 # Newton's Gravity Calculation Logic
 def calculate_gravity(m1, m2, r):
-    G = 6.674e-11
     force = (G * m1 * m2) / (r ** 2)
     return force
 
-# 2. Gravity Calculator Page
-@app.route('/gravity', methods=['GET', 'POST'])
-def gravity():
-    result = None
-    if request.method == 'POST':
-        m1 = float(request.form['mass1'])
-        m2 = float(request.form['mass2'])
-        r = float(request.form['distance'])
-        
-        result = calculate_gravity(m1, m2, r)
-        
-    return render_template('gravity.html', result=result)
+#redius according to gravity function
+def calculate_radius(m1,m2,f):
+    radius=math.sqrt((G * m1 * m2) / f)
+    return radius
 
 #Orbital Velocity calculation logic
 def calculate_orbit(r):
@@ -33,6 +25,33 @@ def calculate_orbit(r):
     M=6e24
     Velocity=math.sqrt(G*M/r)
     return Velocity
+
+# 2.radius/Gravity Calculator Page(according to gravity)
+@app.route('/gravity', methods=['GET', 'POST'])
+def gravity():
+    result_force = None
+    result_radius=None
+    if request.method == 'POST':
+        if 'mass1' in request.form and 'distance' in request.form:
+            try:
+                m1 = float(request.form['mass1'])
+                m2 = float(request.form['mass2'])
+                r = float(request.form['distance'])
+                result_force = calculate_gravity(m1, m2, r)
+
+            except:
+                pass
+
+        if 'gravity' in request.form:
+            try:
+                m1 = float(request.form['mass1'])
+                m2 = float(request.form['mass2'])
+                f = float(request.form['gravity'])
+                result_radius = calculate_radius(m1, m2, f)
+            except:
+                pass
+
+    return render_template('gravity.html', result_force=result_force, result_radius=result_radius)
 
 #Orbit calculation page
 @app.route('/orbit', methods=['GET', 'POST'])
@@ -78,5 +97,24 @@ def centripetal():
     return render_template('centripetal.html', result=result)
 
 
+#Gravitatin time dilation logic
+def g_time(t,m, r):
+    speed_of_light=3e8
+    G=6.674e-11
+    time=t*(math.sqrt(1-(2*G*m/(r*math.pow(speed_of_light,2)))))
+    return time
+
+#Gravitatin time dilation page
+@app.route('/time', methods=['GET','POST'])
+def time():
+    result=None
+    if request.method== 'POST':
+        t=float(request.form['Z_time'])
+        m=float(request.form['Mass'])
+        r=float(request.form['Rdius'])
+        result=g_time(t,m, r)
+    return render_template('time.html', result=result)
+
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10000)
